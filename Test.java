951e893ab3bfef4088794d3fb6bd8df11b56c622
katherine barber <katherine.m.barber@outlook.com>
2022-04-19 22:53:26 -0700

    initial submit

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Kate Barber");
+        System.out.println("I am so many years old");
+        System.out.println("My hometown is Phoenix, AZ");
+    }
+}
